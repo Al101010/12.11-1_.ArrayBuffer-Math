bcebbc631598249fba8fa1377a8e2e084b587587
alexgithub <alexgithub@yandex.ru>
2024-04-11 11:46:54 +1000

    commit-1 100%

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 12.11-1_.ArrayBuffer-Math
+Домашнее задание к лекции «ArrayBuffer»
+1. Math (log/trig)
---
+++ src/js/Math.js
@@ -0,0 +1,23 @@
+// export default class Math {
+//     constructor() {
+//         this.stoned = false;
+//         this.distance = 1;
+//     }
+
+//     stonedSet(power) {
+//         this.stoned = power;
+//     }
+
+//     stonedGet(power) {
+//         return this.stoned;
+//     }
+
+//     attackSet(power) {
+//         this.attack = power;
+//     }
+
+//     attackGet() {
+//         const getAttack = this.attack - Math.log2(x) * 5; // attack - Math.log2(x) * 5
+
+//     }
+// }
